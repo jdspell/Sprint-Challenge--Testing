171ab00e4cb78daeea85cb861f23f855d3f60c79
jdspell <spelljd15@gmail.com>
2019-04-12 12:19:03 -0400

    created get and post endpoints and added three tests for each

--- api/server.js
+++ api/server.js
@@ -9,4 +9,28 @@ server.get('/', async (req, res) => {
     res.status(200).json({ message: "working" });
 });
 
+server.get('/games', async (req, res) => {
+    try {
+        const games = await db('games');
+        res.status(200).json(games);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+server.post('/games', async (req, res) => {
+    try {
+        const { title, genre, releaseYear} = req.body;
+        if(title && genre){
+            const [id] = await db('games').insert(req.body);
+            const game = await db('games').where({ id }).first();
+            res.status(200).json(game);
+        } else {
+            res.status(422).json({ message: "Missing required field." });
+        }
+    } catch (error) {
+        res.status(500).json(error);
+    }
+})
+
 module.exports = server;
